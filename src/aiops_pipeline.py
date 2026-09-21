e66c5aea0d0769ef48f96c497b31cb414c7a6020
Arjun Agrawal <arjun1234agrawal@gmail.com>
2026-09-21 05:29:15 +0000

    task5 aipipeline issue fixed

--- src/aiops_pipeline.py
+++ src/aiops_pipeline.py
@@ -14,16 +14,22 @@ def load_data(file_path):
 def run_pipeline(file_path):
     data = load_data(file_path)
 
-    # INTENTIONAL ASSESSMENT ISSUE #2
-    producer_topic = EventTopic("service-events")
+    # # INTENTIONAL ASSESSMENT ISSUE #2
+    # producer_topic = EventTopic("service-events")
 
+    # detector = AnomalyDetector()
+    # producer = EventProducer(producer_topic)
+
+    # # INTENTIONAL ASSESSMENT ISSUE #3
+    # consumer_topic = EventTopic("anomaly-events")
+    # consumer = EventConsumer(consumer_topic)
+
+    #fixed topics issue
+    event_topic = EventTopic("anomaly-events")
+    
     detector = AnomalyDetector()
-    producer = EventProducer(producer_topic)
-
-    # INTENTIONAL ASSESSMENT ISSUE #3
-    consumer_topic = EventTopic("anomaly-events")
-    consumer = EventConsumer(consumer_topic)
-
+    producer = EventProducer(event_topic)
+    consumer = EventConsumer(event_topic)
     detected_events = []
 
     for record in data:
